91b78ca86058fdfd43080c1b332df39c51dcf8c0
Sat Naing <satnaingdev@gmail.com>
2022-03-15 23:26:07 +0630

    :bento: add semibold font weight

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,7 +7,7 @@ const Document = () => {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
-          href="https://fonts.googleapis.com/css2?family=Jost:wght@400;500;700&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Jost:wght@400;500;600;700&display=swap"
           rel="stylesheet"
         />
       </Head>
