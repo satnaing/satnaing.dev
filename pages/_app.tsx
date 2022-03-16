6ab51c47ab2a20a9980492fafcb486fa7235fca5
Sat Naing <satnaingdev@gmail.com>
2022-03-16 21:51:04 +0630

    :sparkles: implement darkmode provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,13 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import { ThemeProvider } from "next-themes";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider attribute="class">
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
