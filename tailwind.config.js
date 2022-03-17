5ac45e12ce66f0432d89a18fc0363c0d6e2f2efe
Sat Naing <satnaingdev@gmail.com>
2022-03-17 13:04:11 +0630

    :wrench: add custom breakpoints

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,13 @@ module.exports = {
     "./sections/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    screens: {
+      xs: "480px",
+      sm: "640px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
     fontFamily: {
       sans: ['"Jost"', "sans-serif"],
     },
