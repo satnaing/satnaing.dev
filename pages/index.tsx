78497fbd9b9c13c4ec4d3ab1122fa9379b211590
Sat Naing <satnaingdev@gmail.com>
2022-03-17 23:51:09 +0630

    :sparkles: implement social links in index

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,12 +7,14 @@ import BlogSection from "../sections/BlogSection";
 import ContactSection from "../sections/ContactSection";
 import Footer from "../components/Footer";
 import SkipToMain from "../components/SkipToMain";
+import SocialLinks from "../components/SocialLinks";
 
 const Home: NextPage = () => {
   return (
     <div className="bg-bglight dark:bg-bgdark">
       <SkipToMain />
       <Header />
+      <SocialLinks />
       <main id="main">
         <HeroSection />
         <AboutSection />
