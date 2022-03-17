a964b7712df76fae0ca3cadd79b2cdcc1d859c5e
Sat Naing <satnaingdev@gmail.com>
2022-03-17 13:06:16 +0630

    :iphone: update header to be responsive

--- components/Header.tsx
+++ components/Header.tsx
@@ -79,44 +79,63 @@ const Header: React.FC = () => {
   }, [scroll.y, scroll.lastY]);
 
   return (
-    <header>
+    <header className="md:flex">
       <div
-        className={`bg-bglight dark:bg-bgdark z-30 top-0 drop-shadow-md fixed transition-transform duration-400 px-4 h-16 w-full flex items-center justify-between ${navClassList.join(
+        className={`bg-bglight dark:bg-bgdark z-30 top-0 drop-shadow-md fixed transition-transform duration-400 px-4 md:px-8 h-16 w-full ${navClassList.join(
           " "
         )}`}
       >
-        <Link href="/">
-          <a className="text-xl">
-            SatNaing
-            <span className="text-marrsgreen dark:text-carrigreen">.dev</span>
-          </a>
-        </Link>
-        <button
-          type="button"
-          onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
-          aria-label="change theme"
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            className="fill-textlight hidden dark:inline-block"
-          >
-            <path d="M6.993 12c0 2.761 2.246 5.007 5.007 5.007s5.007-2.246 5.007-5.007S14.761 6.993 12 6.993 6.993 9.239 6.993 12zM12 8.993c1.658 0 3.007 1.349 3.007 3.007S13.658 15.007 12 15.007 8.993 13.658 8.993 12 10.342 8.993 12 8.993zM10.998 19h2v3h-2zm0-17h2v3h-2zm-9 9h3v2h-3zm17 0h3v2h-3zM4.219 18.363l2.12-2.122 1.415 1.414-2.12 2.122zM16.24 6.344l2.122-2.122 1.414 1.414-2.122 2.122zM6.342 7.759 4.22 5.637l1.415-1.414 2.12 2.122zm13.434 10.605-1.414 1.414-2.122-2.122 1.414-1.414z" />
-          </svg>
+        <div className="w-full h-full mx-auto max-w-6xl flex items-center justify-between">
+          <Link href="/">
+            <a className="text-xl sm:text-2xl">
+              SatNaing
+              <span className="text-marrsgreen dark:text-carrigreen">.dev</span>
+            </a>
+          </Link>
+          <div className="flex">
+            <nav className="hidden md:block">
+              <ul className="flex">
+                {navLinks.map((navLink) => (
+                  <li key={navLink.url}>
+                    <a
+                      href={navLink.url}
+                      className="text-lg flex flex-col items-center mr-6"
+                    >
+                      {navLink.text}
+                    </a>
+                  </li>
+                ))}
+              </ul>
+            </nav>
+            <button
+              type="button"
+              onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+              aria-label="change theme"
+              className="w-8 h-8 rounded-lg flex justify-center items-center"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                className="fill-textlight hidden dark:inline-block transform scale-110"
+              >
+                <path d="M6.993 12c0 2.761 2.246 5.007 5.007 5.007s5.007-2.246 5.007-5.007S14.761 6.993 12 6.993 6.993 9.239 6.993 12zM12 8.993c1.658 0 3.007 1.349 3.007 3.007S13.658 15.007 12 15.007 8.993 13.658 8.993 12 10.342 8.993 12 8.993zM10.998 19h2v3h-2zm0-17h2v3h-2zm-9 9h3v2h-3zm17 0h3v2h-3zM4.219 18.363l2.12-2.122 1.415 1.414-2.12 2.122zM16.24 6.344l2.122-2.122 1.414 1.414-2.122 2.122zM6.342 7.759 4.22 5.637l1.415-1.414 2.12 2.122zm13.434 10.605-1.414 1.414-2.122-2.122 1.414-1.414z" />
+              </svg>
 
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            className="dark:hidden"
-          >
-            <path d="M20.742 13.045a8.088 8.088 0 0 1-2.077.271c-2.135 0-4.14-.83-5.646-2.336a8.025 8.025 0 0 1-2.064-7.723A1 1 0 0 0 9.73 2.034a10.014 10.014 0 0 0-4.489 2.582c-3.898 3.898-3.898 10.243 0 14.143a9.937 9.937 0 0 0 7.072 2.93 9.93 9.93 0 0 0 7.07-2.929 10.007 10.007 0 0 0 2.583-4.491 1.001 1.001 0 0 0-1.224-1.224zm-2.772 4.301a7.947 7.947 0 0 1-5.656 2.343 7.953 7.953 0 0 1-5.658-2.344c-3.118-3.119-3.118-8.195 0-11.314a7.923 7.923 0 0 1 2.06-1.483 10.027 10.027 0 0 0 2.89 7.848 9.972 9.972 0 0 0 7.848 2.891 8.036 8.036 0 0 1-1.484 2.059z"></path>
-          </svg>
-        </button>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                className="dark:hidden transform scale-90"
+              >
+                <path d="M20.742 13.045a8.088 8.088 0 0 1-2.077.271c-2.135 0-4.14-.83-5.646-2.336a8.025 8.025 0 0 1-2.064-7.723A1 1 0 0 0 9.73 2.034a10.014 10.014 0 0 0-4.489 2.582c-3.898 3.898-3.898 10.243 0 14.143a9.937 9.937 0 0 0 7.072 2.93 9.93 9.93 0 0 0 7.07-2.929 10.007 10.007 0 0 0 2.583-4.491 1.001 1.001 0 0 0-1.224-1.224zm-2.772 4.301a7.947 7.947 0 0 1-5.656 2.343 7.953 7.953 0 0 1-5.658-2.344c-3.118-3.119-3.118-8.195 0-11.314a7.923 7.923 0 0 1 2.06-1.483 10.027 10.027 0 0 0 2.89 7.848 9.972 9.972 0 0 0 7.848 2.891 8.036 8.036 0 0 1-1.484 2.059z"></path>
+              </svg>
+            </button>
+          </div>
+        </div>
       </div>
 
-      <nav className="fixed bottom-4 z-30 left-1/2 transform -translate-x-1/2 bg-bglight dark:bg-carddark dark:text-textlight w-11/12 rounded drop-shadow-lg">
+      <nav className="fixed md:hidden bottom-4 z-30 left-1/2 transform -translate-x-1/2 bg-bglight dark:bg-carddark dark:text-textlight w-11/12 rounded drop-shadow-lg">
         <ul className="flex justify-evenly items-center py-1">
           {navLinks.map((navLink) => (
             <li key={navLink.url}>
@@ -131,12 +150,6 @@ const Header: React.FC = () => {
           ))}
         </ul>
       </nav>
-
-      <button type="button" className="hidden" aria-label="change theme">
-        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24">
-          <path d="M6.993 12c0 2.761 2.246 5.007 5.007 5.007s5.007-2.246 5.007-5.007S14.761 6.993 12 6.993 6.993 9.239 6.993 12zM12 8.993c1.658 0 3.007 1.349 3.007 3.007S13.658 15.007 12 15.007 8.993 13.658 8.993 12 10.342 8.993 12 8.993zM10.998 19h2v3h-2zm0-17h2v3h-2zm-9 9h3v2h-3zm17 0h3v2h-3zM4.219 18.363l2.12-2.122 1.415 1.414-2.12 2.122zM16.24 6.344l2.122-2.122 1.414 1.414-2.122 2.122zM6.342 7.759 4.22 5.637l1.415-1.414 2.12 2.122zm13.434 10.605-1.414 1.414-2.122-2.122 1.414-1.414z" />
-        </svg>
-      </button>
     </header>
   );
 };
