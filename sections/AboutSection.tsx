ee894d46be8d4dda8d1e0bb605f7dfe70b9f0fd8
Sat Naing <satnaingdev@gmail.com>
2022-03-17 13:42:24 +0630

    :iphone: update about section layout for bigger devices

--- sections/AboutSection.tsx
+++ sections/AboutSection.tsx
@@ -25,73 +25,81 @@ const AboutSection: React.FC = () => {
             </h2>
           </RoughNotation>
         </div>
-        <p className="my-2">
-          Hello, my name's Sat Naing. I've graduated from Edinburgh Napier
-          University, majoring in Bachelor of Science (Hons) in Computing. I
-          studied at Info Myanmar University specializing in Software
-          Engineering.
-        </p>
-        <p className="my-2">
-          I have over 2 and a helf years of experience working with web
-          application development_ including both frontend and backend. I'm
-          currently working as a lead developer in a Singapore company.
-        </p>
-        <span>Here are some technologies I've been working with recently</span>
-        <div className="flex my-4" ref={elementRef}>
-          <ul
-            role="list"
-            className="marker:text-marrsgreen dark:marker:text-carrigreen list-disc pl-5 space-y-3"
-          >
-            <li>JavaScript</li>
-            <li>TypeScript</li>
-            <li>React, NextJS</li>
-            <li>Prisma</li>
-            <li>PostgreSQL</li>
-          </ul>
-          <ul
-            role="list"
-            className="marker:text-marrsgreen dark:marker:text-carrigreen list-disc pl-20 space-y-3"
-          >
-            <li>PHP</li>
-            <li>CodeIgniter</li>
-            <li>MySQL</li>
-            <li>Docker</li>
-            <li>CI/CD</li>
-          </ul>
-        </div>
-
-        <div className="text-center">
-          <RoughNotation
-            type="underline"
-            color={`${
-              theme === "light" ? "rgb(0, 140, 140)" : "rgb(5 206 145)"
-            }`}
-            strokeWidth={2}
-            order={2}
-          >
-            <h3 className="text-xl font-medium text-marrsgreen dark:text-carrigreen inline-block mt-12 mb-6">
-              My Education Background
-            </h3>
-          </RoughNotation>
-        </div>
-        {educationInfo.map((edu) => (
-          <div className="mb-4" key={edu.id}>
-            <h4 className="text-marrsgreen dark:text-carrigreen text-lg font-medium">
-              {edu.title}
-            </h4>
-            <span className="text-slate-500 dark:text-slate-200 italic">
-              {edu.subTitle}
+        <div className="md:flex sm:space-x-8">
+          <div className="basis-1/2 xl:basis-3/5">
+            <p className="my-2">
+              Hello, my name's Sat Naing. I've graduated from Edinburgh Napier
+              University, majoring in Bachelor of Science (Hons) in Computing. I
+              studied at Info Myanmar University specializing in Software
+              Engineering.
+            </p>
+            <p className="my-2">
+              I have over 2 and a helf years of experience working with web
+              application development_ including both frontend and backend. I'm
+              currently working as a lead developer in a Singapore company.
+            </p>
+            <span>
+              Here are some technologies I've been working with recently
             </span>
-            <ul
-              role="list"
-              className="marker:text-marrsgreen dark:marker:text-carrigreen list-disc pl-6 space-y-1 mt-1"
-            >
-              {edu.list.map((li) => (
-                <li key={li}>{li}</li>
-              ))}
-            </ul>
+            <div className="flex my-4" ref={elementRef}>
+              <ul
+                role="list"
+                className="marker:text-marrsgreen dark:marker:text-carrigreen list-disc pl-5 space-y-3"
+              >
+                <li>JavaScript</li>
+                <li>TypeScript</li>
+                <li>React, NextJS</li>
+                <li>Prisma</li>
+                <li>PostgreSQL</li>
+              </ul>
+              <ul
+                role="list"
+                className="marker:text-marrsgreen dark:marker:text-carrigreen list-disc pl-20 space-y-3"
+              >
+                <li>PHP</li>
+                <li>CodeIgniter</li>
+                <li>MySQL</li>
+                <li>Docker</li>
+                <li>CI/CD</li>
+              </ul>
+            </div>
           </div>
-        ))}
+
+          <div className="basis-1/2 xl:basis-2/5">
+            <div className="text-center md:hidden">
+              <RoughNotation
+                type="underline"
+                color={`${
+                  theme === "light" ? "rgb(0, 140, 140)" : "rgb(5 206 145)"
+                }`}
+                strokeWidth={2}
+                order={2}
+              >
+                <h3 className="text-xl font-medium text-marrsgreen dark:text-carrigreen inline-block mt-12 mb-6">
+                  My Education Background
+                </h3>
+              </RoughNotation>
+            </div>
+            {educationInfo.map((edu) => (
+              <div className="mb-4" key={edu.id}>
+                <h4 className="text-marrsgreen dark:text-carrigreen text-lg font-medium">
+                  {edu.title}
+                </h4>
+                <span className="text-slate-500 dark:text-slate-200 italic">
+                  {edu.subTitle}
+                </span>
+                <ul
+                  role="list"
+                  className="marker:text-marrsgreen dark:marker:text-carrigreen list-disc pl-6 space-y-1 mt-1"
+                >
+                  {edu.list.map((li) => (
+                    <li key={li}>{li}</li>
+                  ))}
+                </ul>
+              </div>
+            ))}
+          </div>
+        </div>
       </RoughNotationGroup>
     </section>
   );
