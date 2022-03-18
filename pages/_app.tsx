a402b1899a5b282c2268a8fdf9fb67979bf73bb5
Sat Naing <satnaingdev@gmail.com>
2022-03-18 12:49:59 +0630

    :bug: replace viewport tag inside from doc to app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,18 @@
 import type { AppProps } from "next/app";
 import { ThemeProvider } from "next-themes";
 import "../styles/globals.css";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider attribute="class">
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <ThemeProvider attribute="class">
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
   );
 }
 
