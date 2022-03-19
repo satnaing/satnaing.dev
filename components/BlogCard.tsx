a5ac92f3b6283821f3bde2b897adb1185eed89b4
Sat Naing <satnaingdev@gmail.com>
2022-03-19 23:55:37 +0630

    :art: update blog card for new dynamic post da

--- components/BlogCard.tsx
+++ components/BlogCard.tsx
@@ -1,20 +1,24 @@
+import Link from "next/link";
+
 type Props = {
   post: {
-    id: number;
+    slug: string;
     title: string;
-    link: string;
-    desc: string;
+    excerpt: string;
+    datetime: string;
   };
 };
 
 const BlogCard: React.FC<Props> = ({ post }) => {
-  const { title, link, desc } = post;
+  const { title, slug, excerpt, datetime } = post;
   return (
     <div className="my-4 md:mt-0 md:mb-8">
-      <a href={link} className="link inline-block">
-        <h3 className="text-lg font-medium">{title}</h3>
-      </a>
-      <p className="dark:text-gray-300">{desc}</p>
+      <Link href={`/blog/posts/${slug}`}>
+        <a className="link inline-block">
+          <h3 className="text-lg font-medium">{title}</h3>
+        </a>
+      </Link>
+      <p className="dark:text-gray-300">{excerpt}</p>
     </div>
   );
 };
