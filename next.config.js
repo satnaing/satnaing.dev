28b34412008e1ceb6d86a8522ae615c859c6e87a
Sat Naing <satnaingdev@gmail.com>
2022-03-19 18:03:14 +0630

    :wrench: update config to include cloudinary image domain

--- next.config.js
+++ next.config.js
@@ -14,6 +14,9 @@ module.exports = withMDX({
   // Append the default value with md extensions
   pageExtensions: ["ts", "tsx", "js", "jsx", "md", "mdx"],
   reactStrictMode: true,
+  images: {
+    domains: ["res.cloudinary.com"],
+  },
 });
 
 // const nextConfig = {
