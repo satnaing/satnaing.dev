0e60b4e2c91fcffc131c4ab88c9d4897b6ef0720
Sat Naing <satnaingdev@gmail.com>
2022-03-20 10:38:22 +0630

    :heavy_minus_sign: remove next mdx package

--- next.config.js
+++ next.config.js
@@ -1,23 +1,13 @@
 /** @type {import('next').NextConfig} */
 
-const withMDX = require("@next/mdx")({
-  extension: /\.mdx?$/,
-  options: {
-    remarkPlugins: [],
-    rehypePlugins: [],
-    // If you use `MDXProvider`, uncomment the following line.
-    providerImportSource: "@mdx-js/react",
-  },
-});
-
-module.exports = withMDX({
+module.exports = {
   // Append the default value with md extensions
   pageExtensions: ["ts", "tsx", "js", "jsx", "md", "mdx"],
   reactStrictMode: true,
   images: {
     domains: ["res.cloudinary.com"],
   },
-});
+};
 
 // const nextConfig = {
 //   reactStrictMode: true,
