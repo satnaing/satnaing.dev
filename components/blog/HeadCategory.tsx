9dde65f6be34397360ec519a0a94a4f0d6244649
Sat Naing <satnaingdev@gmail.com>
2022-03-20 22:04:16 +0630

    :art: slugify category in url

--- components/blog/HeadCategory.tsx
+++ components/blog/HeadCategory.tsx
@@ -1,19 +1,27 @@
 import Link from "next/link";
 
 type Props = {
-  category: { name?: string; slug?: string };
+  category: string;
 };
 
 const HeadCategory: React.FC<Props> = ({ category }) => {
-  const { name, slug } = category;
+  const slug = slugify(category);
   return (
     <span className="mt-2 mb-2 inline-block px-2 border-l-4 border-marrsgreen">
       Category:{" "}
       <Link href={slug ? `/blog/categories/${slug}` : "uncategorized"}>
-        <a>{name ? name : "uncategorized"}</a>
+        <a>{category ? category : "uncategorized"}</a>
       </Link>
     </span>
   );
 };
+
+const slugify = (str: string) =>
+  str
+    .toLowerCase()
+    .trim()
+    .replace(/[^\w\s-]/g, "")
+    .replace(/[\s_-]+/g, "-")
+    .replace(/^-+|-+$/g, "");
 
 export default HeadCategory;
