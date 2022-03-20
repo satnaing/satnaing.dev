3b3487d0537f48a728b1e9b30b688bb2784786ad
Sat Naing <satnaingdev@gmail.com>
2022-03-21 00:42:05 +0630

    :wrench: add admin redirect

--- next.config.js
+++ next.config.js
@@ -1,10 +1,20 @@
 /** @type {import('next').NextConfig} */
 
 module.exports = {
+  async redirects() {
+    return [
+      {
+        source: "/admin",
+        destination: "https://app.forestry.io/login",
+        permanent: true,
+        basePath: false,
+      },
+    ];
+  },
   // Append the default value with md extensions
   pageExtensions: ["ts", "tsx", "js", "jsx", "md", "mdx", "html"],
   reactStrictMode: true,
-  trailingSlash: true,
+  trailingSlash: false,
   images: {
     domains: ["res.cloudinary.com"],
   },
