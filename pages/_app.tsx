93a31e7f3caa9007a25e5762b43aafc68ce020d2
Sat Naing <satnaingdev@gmail.com>
2022-03-21 19:59:06 +0630

    :sparkles: add search context

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import type { AppProps } from "next/app";
 import { ThemeProvider } from "next-themes";
 import "../styles/globals.css";
 import Head from "next/head";
+import { ProvideSearch } from "context/search";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
@@ -10,7 +11,9 @@ function MyApp({ Component, pageProps }: AppProps) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
       <ThemeProvider attribute="class">
-        <Component {...pageProps} />
+        <ProvideSearch>
+          <Component {...pageProps} />
+        </ProvideSearch>
       </ThemeProvider>
     </>
   );
