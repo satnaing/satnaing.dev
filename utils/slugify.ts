531bc03ea199ee668960523f7cbb0a40d8b717ae
Sat Naing <satnaingdev@gmail.com>
2022-03-21 13:24:45 +0630

    :art: extract slugify as util func

---
+++ utils/slugify.ts
@@ -0,0 +1,9 @@
+const slugify = (str: string) =>
+  str
+    .toLowerCase()
+    .trim()
+    .replace(/[^\w\s-]/g, "")
+    .replace(/[\s_-]+/g, "-")
+    .replace(/^-+|-+$/g, "");
+
+export default slugify;
