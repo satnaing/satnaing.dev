2b9158e184cd00a7c547b102b45d2e134b639e99
Sat Naing <satnaingdev@gmail.com>
2022-03-25 23:10:36 +0630

    :lipstick: update heart emoji to svg

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -13,7 +13,24 @@ const Footer: React.FC = () => {
           </a>
         ))}
       </div>
-      <div>Coded with 🖤 by Sat Naing</div>
+      <div className="flex items-center justify-center">
+        Coded with
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-5 w-5 mx-1 inline-block"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+          strokeWidth={2}
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
+          />
+        </svg>
+        by Sat Naing
+      </div>
     </footer>
   );
 };
