30546b2eb703a1bbbe6e277c66ef96bf0ca338b1
Sat Naing <satnaingdev@gmail.com>
2022-03-25 23:15:42 +0630

    :lipstick: update blog section layout

--- sections/BlogSection.tsx
+++ sections/BlogSection.tsx
@@ -8,6 +8,7 @@ import useOnScreen from "../hooks/useOnScreen";
 import memojiLaptop from "../public/memoji-laptop.png";
 import Link from "next/link";
 import BlogCard from "../components/BlogCard";
+import BlogCardBox from "@/components/BlogCardBox";
 
 const BlogSection: React.FC = () => {
   const elementRef = useRef<HTMLDivElement>(null);
@@ -26,54 +27,42 @@ const BlogSection: React.FC = () => {
           <h2 className="section-heading">Blog</h2>
         </RoughNotation>
       </div>
-      <div className="md:flex md:gap-x-8">
-        <div className="md:basis-1/2 md:flex md:justify-end md:flex-col-reverse">
-          <div className="text-center px-10 md:px-16 lg:px-32">
-            <Image
-              src={memojiLaptop}
-              width={421}
-              height={421}
-              alt="Sat Naing with Laptop memoji"
+      <div className="text-center mb-8" ref={elementRef}>
+        I write blog posts about what I've done and what I'm doing{" "}
+        <br className="hidden sm:block" />
+        as a documenting practice. Here are some of my recent blog posts.
+      </div>
+      <div>
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 sm:grid-rows-2 lg:grid-rows-1 gap-4 md:gap-6 justify-items-center mb-6">
+          {blogPosts.map((post, index) => (
+            <BlogCardBox
+              post={post}
+              className={`${index > 3 ? "hidden lg:block" : ""}`}
+              key={post.slug}
+              fullWH
             />
-          </div>
-          <div className="mb-4" ref={elementRef}>
-            <p>
-              At first, I didn't have any intention to make a blog. But then, I
-              decided to write blog posts for what I've done and what I'm doing
-              as a documenting practice. In the future, I may occationally share
-              some of my knowledge during my professional software developer
-              career.
-            </p>
-            <span className="block my-2 md:hidden">
-              Here are some of my latest blog posts
-            </span>
-          </div>
-        </div>
-        <div className="md:basis-1/2">
-          {blogPosts.map((post) => (
-            <BlogCard post={post} key={post.slug} />
           ))}
-          <div className="mt-4">
-            <Link href="/blog">
-              <a className="link">
-                Read all posts{" "}
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-6 w-6 inline-block"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                  strokeWidth={2}
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M13 7l5 5m0 0l-5 5m5-5H6"
-                  />
-                </svg>
-              </a>
-            </Link>
-          </div>
+        </div>
+        <div className="mt-4 text-center">
+          <Link href="/blog">
+            <a className="link">
+              Read all posts{" "}
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6 inline-block"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                strokeWidth={2}
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M13 7l5 5m0 0l-5 5m5-5H6"
+                />
+              </svg>
+            </a>
+          </Link>
         </div>
       </div>
     </section>
@@ -97,6 +86,27 @@ const blogPosts = [
   },
   {
     slug: "Firebase",
+    title: "From Firebase/Redis to MySQL with PlanetScale",
+    datetime: "3/19/2022, 9:09:46 PM",
+    excerpt:
+      "Learn how I migrated my Next.js website to use MySQL with PlanetScale, resulting in 10x faster response times for my APIs.",
+  },
+  {
+    slug: "Firebase 2",
+    title: "From Firebase/Redis to MySQL with PlanetScale",
+    datetime: "3/19/2022, 9:09:46 PM",
+    excerpt:
+      "Learn how I migrated my Next.js website to use MySQL with PlanetScale, resulting in 10x faster response times for my APIs.",
+  },
+  {
+    slug: "Firebase 3",
+    title: "From Firebase/Redis to MySQL with PlanetScale",
+    datetime: "3/19/2022, 9:09:46 PM",
+    excerpt:
+      "Learn how I migrated my Next.js website to use MySQL with PlanetScale, resulting in 10x faster response times for my APIs.",
+  },
+  {
+    slug: "Firebase 4",
     title: "From Firebase/Redis to MySQL with PlanetScale",
     datetime: "3/19/2022, 9:09:46 PM",
     excerpt:
