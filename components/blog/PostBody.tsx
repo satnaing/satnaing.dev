faea3e2d48208ec4ba899c88e4063c179268bf4a
Sat Naing <satnaingdev@gmail.com>
2022-03-26 15:54:21 +0630

    :lipstick: update prose p margin

--- components/blog/PostBody.tsx
+++ components/blog/PostBody.tsx
@@ -4,7 +4,7 @@ type Props = {
 
 const PostBody = ({ content }: Props) => {
   return (
-    <div className="prose prose-lg prose-headings:font-medium prose-h2:mb-3 prose-h3:mb-1 prose-h3:mt-6 prose-h3:text-xl prose-h3:font-bold prose-h3:italic prose-h3:text-marrsgreen dark:prose-h3:text-carrigreen prose-p:my-2 mx-auto max-w-4xl dark:prose-invert">
+    <div className="prose prose-lg prose-headings:font-medium prose-h2:mb-3 prose-h3:mb-1 prose-h3:mt-6 prose-h3:text-xl prose-h3:font-bold prose-h3:italic prose-h3:text-marrsgreen dark:prose-h3:text-carrigreen prose-p:mt-1 prose-p:my-3 mx-auto max-w-4xl dark:prose-invert">
       <div
         // className={markdownStyles["markdown"]}
         dangerouslySetInnerHTML={{ __html: content }}
