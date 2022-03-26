5d127fe62b38f62032f13397e3ed111acfccfe49
Sat Naing <satnaingdev@gmail.com>
2022-03-26 16:00:33 +0630

    :lipstick: update portfolio menu icon

--- components/blog/BlogHeader.tsx
+++ components/blog/BlogHeader.tsx
@@ -131,8 +131,7 @@ const navLinks = [
         height="24"
         className="dark:fill-textlight"
       >
-        <path d="m13 3 3.293 3.293-7 7 1.414 1.414 7-7L21 11V3z"></path>
-        <path d="M19 19H5V5h7l-2-2H5c-1.103 0-2 .897-2 2v14c0 1.103.897 2 2 2h14c1.103 0 2-.897 2-2v-5l-2-2v7z"></path>
+        <path d="M12 2a5 5 0 1 0 5 5 5 5 0 0 0-5-5zm0 8a3 3 0 1 1 3-3 3 3 0 0 1-3 3zm9 11v-1a7 7 0 0 0-7-7h-4a7 7 0 0 0-7 7v1h2v-1a5 5 0 0 1 5-5h4a5 5 0 0 1 5 5v1z"></path>
       </svg>
     ),
     text: "Portfolio",
