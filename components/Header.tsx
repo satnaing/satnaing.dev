65677a71604461ef976b455d26841f26e25e276c
Sat Naing <satnaingdev@gmail.com>
2022-04-04 22:36:07 +0630

    :lipstick: add active link state style in header

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,17 +2,13 @@ import Link from "next/link";
 import { useEffect, useState } from "react";
 import { useTheme } from "next-themes";
 import useScrollListener from "../hooks/useScrollListener";
+import { useSection } from "context/section";
 
 const navLinks = [
   {
     url: "#whoami",
     svg: (
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="24"
-        height="24"
-        className="dark:fill-textlight"
-      >
+      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24">
         <path d="M12 2a5 5 0 1 0 5 5 5 5 0 0 0-5-5zm0 8a3 3 0 1 1 3-3 3 3 0 0 1-3 3zm9 11v-1a7 7 0 0 0-7-7h-4a7 7 0 0 0-7 7v1h2v-1a5 5 0 0 1 5-5h4a5 5 0 0 1 5 5v1z" />
       </svg>
     ),
@@ -21,12 +17,7 @@ const navLinks = [
   {
     url: "#projects",
     svg: (
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="24"
-        height="24"
-        className="dark:fill-textlight"
-      >
+      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24">
         <path d="m7.375 16.781 1.25-1.562L4.601 12l4.024-3.219-1.25-1.562-5 4a1 1 0 0 0 0 1.562l5 4zm9.25-9.562-1.25 1.562L19.399 12l-4.024 3.219 1.25 1.562 5-4a1 1 0 0 0 0-1.562l-5-4zm-1.649-4.003-4 18-1.953-.434 4-18z" />
       </svg>
     ),
@@ -35,12 +26,7 @@ const navLinks = [
   {
     url: "#blog",
     svg: (
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="24"
-        height="24"
-        className="dark:fill-textlight"
-      >
+      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24">
         <path d="M19.875 3H4.125C2.953 3 2 3.897 2 5v14c0 1.103.953 2 2.125 2h15.75C21.047 21 22 20.103 22 19V5c0-1.103-.953-2-2.125-2zm0 16H4.125c-.057 0-.096-.016-.113-.016-.007 0-.011.002-.012.008L3.988 5.046c.007-.01.052-.046.137-.046h15.75c.079.001.122.028.125.008l.012 13.946c-.007.01-.052.046-.137.046z" />
         <path d="M6 7h6v6H6zm7 8H6v2h12v-2h-4zm1-4h4v2h-4zm0-4h4v2h-4z" />
       </svg>
@@ -50,12 +36,7 @@ const navLinks = [
   {
     url: "#contact",
     svg: (
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="24"
-        height="24"
-        className="dark:fill-textlight"
-      >
+      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24">
         <path d="M20.563 3.34a1.002 1.002 0 0 0-.989-.079l-17 8a1 1 0 0 0 .026 1.821L8 15.445v6.722l5.836-4.168 4.764 2.084a1 1 0 0 0 1.399-.85l1-15a1.005 1.005 0 0 0-.436-.893zm-2.466 14.34-5.269-2.306L16 9.167l-7.649 4.25-2.932-1.283 13.471-6.34-.793 11.886z" />
       </svg>
     ),
@@ -65,18 +46,19 @@ const navLinks = [
 
 const Header: React.FC = () => {
   const { theme, setTheme } = useTheme();
+  const { currentSection } = useSection();
   const [navClassList, setNavClassList] = useState<any>([]);
   const scroll = useScrollListener();
 
   // update classList of nav on scroll
-  useEffect(() => {
-    const _classList = [];
+  // useEffect(() => {
+  //   const _classList = [];
 
-    if (scroll.y > 150 && scroll.y - scroll.lastY > 0)
-      _classList.push("transform -translate-y-full drop-shadow-none");
+  //   if (scroll.y > 150 && scroll.y - scroll.lastY > 0)
+  //     _classList.push("transform -translate-y-full drop-shadow-none");
 
-    setNavClassList(_classList);
-  }, [scroll.y, scroll.lastY]);
+  //   setNavClassList(_classList);
+  // }, [scroll.y, scroll.lastY]);
 
   return (
     <header className="md:flex">
@@ -87,7 +69,7 @@ const Header: React.FC = () => {
       >
         <div className="w-full h-full mx-auto max-w-6xl flex items-center justify-between">
           <Link href="/">
-            <a className="text-xl sm:text-2xl hover:text-marrsgreen dark:hover:text-carrigreen focus-visible:outline-marrsgreen dark:focus-visible:outline-carrigreen">
+            <a className="text-xl sm:text-2xl md:hover:text-marrsgreen dark:md:hover:text-carrigreen focus-visible:outline-marrsgreen dark:focus-visible:outline-carrigreen">
               SatNaing
               <span className="text-marrsgreen dark:text-carrigreen">.dev</span>
             </a>
@@ -99,7 +81,10 @@ const Header: React.FC = () => {
                   <li key={navLink.url}>
                     <a
                       href={navLink.url}
-                      className="text-lg flex flex-col items-center mr-6 hover:text-marrsgreen dark:hover:text-carrigreen link-outline"
+                      className={`text-lg flex flex-col items-center mr-6 hover:text-marrsgreen dark:hover:text-carrigreen link-outline ${
+                        currentSection === navLink.text.toLocaleLowerCase() &&
+                        "text-marrsgreen dark:text-carrigreen"
+                      }`}
                     >
                       {navLink.text}
                     </a>
@@ -141,7 +126,10 @@ const Header: React.FC = () => {
             <li key={navLink.url}>
               <a
                 href={navLink.url}
-                className="text-sm flex flex-col items-center w-[4.5rem]"
+                className={`text-sm flex flex-col items-center w-[4.5rem] dark:fill-textlight ${
+                  currentSection === navLink.text.toLocaleLowerCase() &&
+                  "text-marrsgreen dark:text-carrigreen fill-marrsgreen dark:fill-carrigreen"
+                }`}
               >
                 {navLink.svg}
                 {navLink.text}
