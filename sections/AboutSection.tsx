d78e9f03555a6d03603f4a1257bc9324c27490b1
Sat Naing <satnaingdev@gmail.com>
2022-04-26 00:38:05 +0630

    :lipstick: update about section layout

--- sections/AboutSection.tsx
+++ sections/AboutSection.tsx
@@ -5,18 +5,45 @@ import { useTheme } from "next-themes";
 import { useSection } from "context/section";
 import useOnScreen from "hooks/useOnScreen";
 import useScrollActive from "hooks/useScrollActive";
+import Image from "next/image";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
+
+import satNaing from "../public/satnaing-about.jpg";
 
 const AboutSection: React.FC = () => {
-  const { theme } = useTheme();
-
   const sectionRef = useRef<HTMLDivElement>(null);
   const isSecOnScreen = useOnScreen(sectionRef);
+
+  const q = gsap.utils.selector(sectionRef);
+
+  useEffect(() => {
+    gsap.registerPlugin(ScrollTrigger);
+
+    gsap.fromTo(
+      q(".profile-picture"),
+      { y: -50 },
+      {
+        scrollTrigger: {
+          trigger: q(".profile-picture"),
+          // toggleActions: "start pause resume reverse",\
+          start: "top 60%",
+          scrub: true,
+          // markers: true,
+        },
+        y: 50,
+        duration: 3,
+      }
+    );
+  }, [q]);
+
+  const { theme } = useTheme();
 
   const eduRef = useRef<HTMLDivElement>(null);
   const isEduOnScreen = useOnScreen(eduRef);
 
-  const elementRef = useRef<HTMLDivElement>(null);
-  const isOnScreen = useOnScreen(elementRef);
+  // const elementRef = useRef<HTMLDivElement>(null);
+  // const isOnScreen = useOnScreen(elementRef);
 
   // Set active link for about section
   const aboutSection = useScrollActive(sectionRef);
@@ -55,103 +82,79 @@ const AboutSection: React.FC = () => {
   };
 
   return (
-    <section ref={sectionRef} id="whoami" className="section">
-      <RoughNotationGroup show={isOnScreen}>
-        <div className="text-center">
-          <RoughNotation
-            type="underline"
-            color={`${
-              theme === "light" ? "rgb(0, 140, 140)" : "rgb(5 206 145)"
-            }`}
-            strokeWidth={2}
-            order={1}
-          >
-            <h2 className="section-heading">Who am I?</h2>
-          </RoughNotation>
-        </div>
-        <div className="md:flex md:space-x-8">
-          <div className="basis-1/2 xl:basis-3/5">
-            <p className="my-2">
-              Hello, my name's Sat Naing. I've graduated from{" "}
-              <a href="https://www.napier.ac.uk/" className="link">
-                Edinburgh Napier University
-              </a>
-              , majoring in Bachelor of Science (Hons) in Computing. I studied
-              at{" "}
-              <a href="https://imu.edu.mm/" className="link">
-                Info Myanmar University
-              </a>{" "}
-              specializing in Software Engineering.
-            </p>
-            <p className="my-2">
-              I have over 2 and a half years of experience working with web
-              application development_ including both frontend and backend. I'm
-              currently working as a lead developer at a Singapore company.
-            </p>
-            <span>
-              Here are some technologies I've been working with recently
-            </span>
-            <div className="flex my-4" ref={elementRef}>
-              <ul
-                role="list"
-                className="marker:text-marrsgreen dark:marker:text-carrigreen list-disc pl-5 space-y-3"
-              >
-                <li>JavaScript</li>
-                <li>TypeScript</li>
-                <li>React, NextJS</li>
-                <li>Prisma</li>
-                <li>PostgreSQL</li>
-              </ul>
-              <ul
-                role="list"
-                className="marker:text-marrsgreen dark:marker:text-carrigreen list-disc pl-20 space-y-3"
-              >
-                <li>PHP</li>
-                <li>CodeIgniter</li>
-                <li>MySQL</li>
-                <li>Docker</li>
-                <li>CI/CD</li>
-              </ul>
-            </div>
+    <div className="bg-white dark:bg-[#1B2731]">
+      <section ref={sectionRef} id="whoami" className="section">
+        <RoughNotationGroup>
+          <div className="text-center">
+            <RoughNotation
+              type="underline"
+              color={`${
+                theme === "light" ? "rgb(0, 140, 140)" : "rgb(5 206 145)"
+              }`}
+              strokeWidth={2}
+              order={1}
+            >
+              <h2 className="section-heading">Who am I?</h2>
+            </RoughNotation>
           </div>
-
-          <div className="basis-1/2 xl:basis-2/5" ref={eduRef}>
-            <div className="text-center md:sr-only">
-              <RoughNotation
-                type="underline"
-                color={`${
-                  theme === "light" ? "rgb(0, 140, 140)" : "rgb(5 206 145)"
-                }`}
-                strokeWidth={2}
-                order={2}
-              >
-                <h3 className="text-xl font-medium text-marrsgreen dark:text-carrigreen inline-block mt-12 mb-6">
-                  My Education Background
-                </h3>
-              </RoughNotation>
-            </div>
-            {educationInfo.map((edu) => (
-              <div className="mb-4" key={edu.id}>
-                <h4 className="text-marrsgreen dark:text-carrigreen text-lg font-medium">
-                  {edu.title}
-                </h4>
-                <span className="text-slate-500 dark:text-slate-200 italic">
-                  {edu.subTitle}
-                </span>
-                <ul
-                  role="list"
-                  className="marker:text-marrsgreen dark:marker:text-carrigreen list-disc pl-6 space-y-1 mt-1"
-                >
-                  {edu.list.map((li) => (
-                    <li key={li}>{li}</li>
-                  ))}
-                </ul>
+          <div className="md:flex md:space-x-8">
+            <div className="basis-2/5 lg:basis-1/2 xl:basis-3/5 flex justify-center items-center">
+              <div className="relative w-72 ">
+                {/* <div className="absolute w-full h-full top-4 left-6 border-2 border-marrsgreen"></div> */}
+                <Image
+                  src={satNaing}
+                  width={1080}
+                  height={1348}
+                  priority
+                  alt="Sat Naing profile picture"
+                  className="profile-picture drop-shadow-lg hover:grayscale"
+                />
               </div>
-            ))}
+            </div>
+
+            <div className="basis-3/5 lg:basis-1/2 xl:basis-2/5" ref={eduRef}>
+              {/* <p className="my-2">
+                Hello, my name's Sat Naing. I've graduated from{" "}
+                <a href="https://www.napier.ac.uk/" className="link">
+                  Edinburgh Napier University
+                </a>
+                , majoring in Bachelor of Science (Hons) in Computing. I studied
+                at{" "}
+                <a href="https://imu.edu.mm/" className="link">
+                  Info Myanmar University
+                </a>{" "}
+                specializing in Software Engineering.
+              </p> */}
+              <p className="my-2">
+                I have over 2 and a half years of experience working with web
+                application development_ including both frontend and backend.
+                I'm currently working as a lead developer at a Singapore
+                company.
+              </p>
+              <div className="my-4">Here is my education background.</div>
+              {educationInfo.map((edu) => (
+                <div className="mb-4" key={edu.id}>
+                  <h4 className="text-marrsgreen dark:text-carrigreen text-lg font-medium">
+                    {edu.title}
+                  </h4>
+                  <span className="text-slate-500 dark:text-slate-200 italic">
+                    {edu.subTitle}
+                  </span>
+                  <ul
+                    role="list"
+                    className="marker:text-marrsgreen dark:marker:text-carrigreen list-disc pl-6 space-y-1 mt-1"
+                  >
+                    {edu.list.map((li) => (
+                      <li key={li}>{li}</li>
+                    ))}
+                  </ul>
+                </div>
+              ))}
+            </div>
           </div>
-        </div>
-      </RoughNotationGroup>
-    </section>
+        </RoughNotationGroup>
+      </section>
+    </div>
   );
 };
 
@@ -179,13 +182,7 @@ const educationInfo = [
     id: 3,
     title: "IELTS",
     subTitle: "British Council Myanmar | 2017",
-    list: ["Got overall band score 6.5 and at least 6 in each skill"],
-  },
-  {
-    id: 4,
-    title: "Myanmar Matriculation Exam",
-    subTitle: "B.E.H.S (2) Thingangyun | 2015",
-    list: ["Passed with 3 Distinctions"],
+    list: ["Got overall band score 6.5."],
   },
 ];
 
