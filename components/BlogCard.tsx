284a6c4ca224498fbdf931472ebf105cf8299760
Sat Naing <satnaingdev@gmail.com>
2022-05-19 23:16:55 +0630

    :dizzy: add blog card animation

--- components/BlogCard.tsx
+++ components/BlogCard.tsx
@@ -1,4 +1,8 @@
+import { useEffect, useRef } from "react";
 import Link from "next/link";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
+
 import formatDatetime from "utils/formatDatetime";
 
 type Props = {
@@ -12,31 +16,71 @@ type Props = {
 
 const BlogCard: React.FC<Props> = ({ post }) => {
   const { title, slug, excerpt, datetime } = post;
+
+  const sectionRef = useRef<HTMLDivElement>(null);
+
+  // Animations
+  const q = gsap.utils.selector(sectionRef);
+
+  useEffect(() => {
+    gsap.registerPlugin(ScrollTrigger);
+
+    const tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: sectionRef.current,
+        start: `70% bottom`,
+      },
+    });
+
+    tl.fromTo(
+      q(".blog-title"),
+      { opacity: 0, y: 50 },
+      {
+        opacity: 1,
+        y: 0,
+        ease: "Power3.easeInOut",
+        duration: 0.5,
+        stagger: 0.2,
+      }
+    )
+      .fromTo(q(".blog-datetime"), { y: 100 }, { y: 0, stagger: 0.2 }, "<25%")
+      .fromTo(
+        q(".blog-excerpt"),
+        { opacity: 0 },
+        { opacity: 1, stagger: 0.2, duration: 1.5 },
+        "<10%"
+      );
+  }, [q]);
+
   return (
-    <div className="my-4 md:mt-0 md:mb-8">
-      <Link href={`/blog/posts/${slug}`}>
-        <a className="link inline-block">
-          <h3 className="text-lg font-medium">{title}</h3>
-        </a>
-      </Link>
-      <div className="italic text-sm mb-1 text-carddark dark:text-gray-300 flex items-center">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="h-4 w-4 mr-2"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-          strokeWidth={2}
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"
-          />
-        </svg>
-        {formatDatetime(datetime)}
+    <div ref={sectionRef} className="my-4 md:mt-0 md:mb-8">
+      <div className="overflow-hidden">
+        <Link href={`/blog/posts/${slug}`}>
+          <a className="blog-title link inline-block">
+            <h3 className="text-lg font-medium">{title}</h3>
+          </a>
+        </Link>
       </div>
-      <p className="dark:text-gray-300">{excerpt}</p>
+      <div className="overflow-hidden">
+        <div className="blog-datetime italic text-sm mb-1 text-carddark dark:text-gray-300 flex items-center">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-4 w-4 mr-2"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            strokeWidth={2}
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"
+            />
+          </svg>
+          {formatDatetime(datetime)}
+        </div>
+      </div>
+      <p className="blog-excerpt dark:text-gray-300">{excerpt}</p>
     </div>
   );
 };
