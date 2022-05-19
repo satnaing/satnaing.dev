991b5b6cbd32c14cf4b31484f9737a0c0982d025
Sat Naing <satnaingdev@gmail.com>
2022-05-19 22:54:04 +0630

    :art: replace loader codes with loader component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,7 @@
-import { useEffect, useRef } from "react";
 import type { GetStaticProps, NextPage } from "next";
-import gsap from "gsap";
-import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
 
 import AppHead from "@/components/AppHead";
+import Loader from "@/components/Loader";
 import SkipToMain from "@/components/SkipToMain";
 import Header from "@/components/Header";
 import SocialLinks from "@/components/SocialLinks";
@@ -32,21 +30,6 @@ const meta = {
 };
 
 const Home: NextPage<Props> = ({ blogPosts }) => {
-  const loadingRef = useRef(null);
-  const q = gsap.utils.selector(loadingRef);
-
-  useEffect(() => {
-    gsap.registerPlugin(ScrollTrigger);
-
-    const tl = gsap.timeline({ defaults: { duration: 0.7 } });
-    tl.fromTo(q(".loading-text"), { y: 120 }, { y: -10 });
-    tl.to(q(".white-bg"), { y: "-100%" }).to(
-      q(".dark-bg"),
-      { y: "-100%", duration: 0.6 },
-      "-=0.6"
-    );
-  }, []);
-
   return (
     <>
       <AppHead
@@ -54,16 +37,7 @@ const Home: NextPage<Props> = ({ blogPosts }) => {
         url={`${process.env.NEXT_PUBLIC_URL}`}
         meta={meta}
       />
-      <div ref={loadingRef}>
-        <div className="white-bg fixed top-0 left-0 w-full h-screen bg-[#f0f5fa] dark:bg-[#0e141a] z-[9999] flex justify-center items-center">
-          <div className="overflow-hidden">
-            <span className="loading-text inline-block text-bgdark dark:text-bglight text-4xl sm:text-5xl lg:text-7xl tracking-widest">
-              SatNaing.dev
-            </span>
-          </div>
-        </div>
-        <div className="dark-bg fixed top-0 left-0 w-full h-screen bg-marrsgreen dark:bg-carrigreen z-[9998]"></div>
-      </div>
+      <Loader>SatNaing.dev</Loader>
       <div className="bg-bglight dark:bg-bgdark overflow-hidden">
         <div className="selection:bg-marrsgreen selection:text-bglight dark:selection:bg-carrigreen dark:selection:text-bgdark">
           <SkipToMain />
