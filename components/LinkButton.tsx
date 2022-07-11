25e4e985adabbc8d44a190067ae50a2bd9e5333b
Sat Naing <satnaingdev@gmail.com>
2022-07-11 23:53:07 +0630

    :lipstick: add outline hover style

--- components/LinkButton.tsx
+++ components/LinkButton.tsx
@@ -17,7 +17,7 @@ const LinkButton: React.FC<Props> = ({
       role="button"
       className={`${
         outline
-          ? "border border-marrsgreen dark:border-carrigreen text-marrsgreen dark:text-carrigreen"
+          ? "border border-marrsgreen hover:bg-marrsgreen dark:border-carrigreen dark:hover:bg-carrigreen text-marrsgreen hover:text-cardlight dark:text-carrigreen dark:hover:text-carddark transition"
           : "bg-marrsgreen hover:bg-marrslight active:bg-marrsdark dark:hover:bg-carrilight dark:active:bg-carridark dark:bg-carrigreen text-bglight dark:text-bgdark"
       } py-2 px-3 rounded lg:text-xl ${className} outline-marrsgreen dark:outline-carrigreen focus-visible:outline-double outline-offset-2`}
       href={href}
