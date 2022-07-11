0290238b2defe3e267b227db942ba3982f7ddcb1
Sat Naing <satnaingdev@gmail.com>
2022-07-11 20:58:43 +0630

    :bento: serve profile picture with webp format

--- sections/AboutSection.tsx
+++ sections/AboutSection.tsx
@@ -9,7 +9,7 @@ import { useSection } from "context/section";
 import useOnScreen from "hooks/useOnScreen";
 import useScrollActive from "hooks/useScrollActive";
 
-import satNaing from "../public/satnaing-profile.jpg";
+import satNaing from "../public/satnaing-profile.webp";
 import AboutBgSvg from "@/components/AboutBgSvg";
 import EduGroup from "@/components/EduGroup";
 
