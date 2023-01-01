8c7b62e39cbe0bebf3889ac193a1f77376066907
satnaing <satnaingdev@gmail.com>
2023-01-01 21:49:38 +0630

    :wheelchair: Hide decorative images from a11y tree

--- components/AboutBgSvg.tsx
+++ components/AboutBgSvg.tsx
@@ -7,19 +7,34 @@ import arrow from "../public/extra/arrow.svg";
 const AboutBgSvg: React.FC = () => {
   return (
     <>
-      <span className="bg-svg hidden lg:inline-block absolute bottom-12 -left-12 dark:hidden">
+      <span
+        aria-hidden="true"
+        className="bg-svg hidden lg:inline-block absolute bottom-12 -left-12 dark:hidden"
+      >
         <Image src={dots} width={102} height={153} alt="dots background" />
       </span>
-      <span className="bg-svg absolute bottom-12 -left-12 hidden lg:dark:inline-block">
+      <span
+        aria-hidden="true"
+        className="bg-svg absolute bottom-12 -left-12 hidden lg:dark:inline-block"
+      >
         <Image src={dotsDark} width={102} height={153} alt="dots background" />
       </span>
-      <span className="bg-svg hidden lg:inline-block absolute bottom-96 -right-4 dark:hidden">
+      <span
+        aria-hidden="true"
+        className="bg-svg hidden lg:inline-block absolute bottom-96 -right-4 dark:hidden"
+      >
         <Image src={dots} width={102} height={153} alt="dots background" />
       </span>
-      <span className="bg-svg absolute bottom-96 -right-4 hidden lg:dark:inline-block">
+      <span
+        aria-hidden="true"
+        className="bg-svg absolute bottom-96 -right-4 hidden lg:dark:inline-block"
+      >
         <Image src={dotsDark} width={102} height={153} alt="dots background" />
       </span>
-      <span className="bg-svg hidden lg:inline-block absolute bottom-24 right-44">
+      <span
+        aria-hidden="true"
+        className="bg-svg hidden lg:inline-block absolute bottom-24 right-44"
+      >
         <Image src={arrow} width={19} height={60} alt="up arrow" />
       </span>
     </>
